f0c59f5fe8409bcbcfd7af8f42dcb8b72e1d44a1
nirajninja <nirajgusain5@gmail.com>
2021-05-22 19:37:42 +0530

    init repo and database

--- app/src/main/java/com/example/noteappmvvm/NoteDao.kt
+++ app/src/main/java/com/example/noteappmvvm/NoteDao.kt
@@ -1,16 +1,17 @@
 package com.example.noteappmvvm
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
 interface NoteDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(note: Note)
+    suspend fun insert(note: Note)
 
     @Delete
-    fun delete(note: Note)
+    suspend fun delete(note: Note)
 
     @Query("Select * from notes_table order by id ASC")
-    fun getAllNote():List<Note>
+    fun getAllNote():LiveData<List<Note>>
 }
---
+++ app/src/main/java/com/example/noteappmvvm/NoteRepository.kt
@@ -0,0 +1,15 @@
+package com.example.noteappmvvm
+
+import androidx.lifecycle.LiveData
+
+class NoteRepository (private val noteDao:NoteDao){
+    val allNotes:LiveData<List<Note>>  = noteDao.getAllNote()
+
+    suspend fun insert(note:Note){
+        noteDao.insert(note)
+    }
+    suspend fun delete(note:Note){
+        noteDao.delete(note)
+    }
+
+}
