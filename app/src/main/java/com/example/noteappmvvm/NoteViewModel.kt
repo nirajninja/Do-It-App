a7d252d1e46752b183a4a0e5e19678c98f17a487
nirajninja <nirajgusain5@gmail.com>
2021-05-23 19:45:37 +0530

    init viewModel

---
+++ app/src/main/java/com/example/noteappmvvm/NoteViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.noteappmvvm
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+class NoteViewModel(application: Application):AndroidViewModel(application) {
+
+    val allNotes:LiveData<List<Note>>
+    init {
+
+        val dao = NoteDatabase.getDatabase(application).getDao()
+        val repository=NoteRepository(dao)
+        allNotes=repository.allNotes
+    }
+}
